094002d860151b8e7b87c411acdb9a0ba5baa5e2
Whitney Crawford <whitneylcrawford@gmail.com>
2019-08-18 12:47:37 -0400

    bestMatch url will console but not opening in browser

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,29 +12,32 @@ apiRouter.get("/api/wines", function(req, res) {
 // API POST Request
 apiRouter.post("/api/wines", function(req, res) {
     
-    var userData = req.body;
-    
     var bestMatch = {
         name: "",
         url: "",
         difference: Infinity
-    }
+    };
+    console.log(req.body);
 
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    console.log(userScores);
+    var totDiff = 0;
     for(var i = 0; i < wines.length; i++){
-        var totDiff = 0;
-        for(var j = 0; j < wines[i].scores.length; j++){
-            totDiff += Math.abs(parseInt(wines[i].scores[j]) - parseInt(userData.scores[j]))
+        console.log(wines[i]);
+        totDiff = 0;
+        for(var j = 0; j < wines[i].scores[j]; j++){
+            totDiff += Math.abs(parseInt(userScores[j])- parseInt(wines[i].scores[j]));
         }
-        if(totDiff < bestMatch.difference){
+        if(totDiff <= bestMatch.difference){
             bestMatch.name = wines[i].name
             bestMatch.url = wines[i].url
-            bestMatch.difference = totDiff
+            bestMatch.difference = totDiff;
         }
     }
-
-    wines.push(userData)
-    res.json(bestMatch)
-        
+    // This returns the bestMatch wine object to the frontend in JSON format
+    res.json(bestMatch)        
 });
 
 module.exports = apiRouter;
