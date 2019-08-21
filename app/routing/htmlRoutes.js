2118c7b4630de426caa6aca26325c042b630cb2c
Whitney Crawford <whitneylcrawford@gmail.com>
2019-08-21 12:26:56 -0400

    updates to html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,5 +6,13 @@ var path = require("path");
 htmlRouter.get('/', function(req, res) {
     res.sendFile("home.html", {root: path.join('./app/public')});
 });
+htmlRouter.get('/bio', function (req, res) {
+    res.sendFile("bio.html", {root: path.join('./app/public')});
+  });
+  
+  // If no matching route is found default to home
+  htmlRouter.get("*", function(req, res) {
+    res.sendFile("home.html", {root: path.join('./app/public')});
+  });
 
 module.exports = htmlRouter;
